a6e0d576f7cf392466027250b242191b5c76f1cd
Victor <victor_f_@hotmail.com>
2019-12-01 14:29:41 +0100

    Completed first day

---
+++ src/One/Library.fs
@@ -0,0 +1,52 @@
+﻿namespace One
+
+open Expecto
+
+module Rocket =
+    let calculateFuel mass =
+        let result =
+            mass / 3.
+            |> floor
+        result - 2.
+
+    let rec calculateFuel' mass =
+        let result = calculateFuel mass
+
+        if result <= 0. then
+            0.
+        else 
+            result + (calculateFuel' result)
+        
+
+module Tests =
+    let tests =
+        testList "Calculate Fuel" [
+            test "Mass of 12" {
+                let mass = 12.
+                let result = Rocket.calculateFuel mass
+
+                Expect.equal result 2. "Mass of 12 should use 2 fuel"
+            }
+
+            test "Mass of 14" {
+                let mass = 14.
+                let result = Rocket.calculateFuel mass
+
+                Expect.equal result 2. "Mass of 14 should use 2 fuel"
+            }
+
+            test "Mass of 1969" {
+                let mass = 1969.
+                let result = Rocket.calculateFuel mass
+
+                Expect.equal result 654. "Mass of 1969 should use 654 fuel"
+            }
+
+            test "Mass of 100756" {
+                let mass = 100_756.
+                let result = Rocket.calculateFuel mass
+
+                Expect.equal result 33_583. "Mass of 100756 should use 33583 fuel"
+            }
+
+        ]
--- src/console/Program.fs
+++ src/console/Program.fs
@@ -2,7 +2,24 @@
 
 open System
 
+open Expecto
+open One.Tests
+
+let readInput path = System.IO.File.ReadLines(path)
+
+
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let masses = readInput "./src/console/one_input"
+
+    let result =
+        masses 
+        |> Seq.map float
+        |> Seq.sumBy One.Rocket.calculateFuel'
+
+    printf "Sum is: %f" result
+
+    // runTests defaultConfig One.Tests.tests |> ignore
+    Console.ReadLine() |> ignore
     0 // return an integer exit code
