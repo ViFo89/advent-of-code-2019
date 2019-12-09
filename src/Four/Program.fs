8db924ceb754c1d28acd1e8e38fbfba0d9411dc8
Victor <victor_f_@hotmail.com>
2019-12-09 21:55:09 +0100

    Four - First done

--- src/Four/Program.fs
+++ src/Four/Program.fs
@@ -1,8 +1,25 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open Password
+
+let rec loop start stop acc= 
+    printfn "loop | start: %i | acc: %i" start acc
+    let result = findPasswords start stop
+
+    match result with
+    | Result i -> loop (i + 1) stop (acc + 1) 
+    | NoResult -> acc
+
+
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let start = 108457
+    let stop = 562041
+    let result = loop start stop 0
+
+    printfn "Result is %i" result
+    
+    
     0 // return an integer exit code
